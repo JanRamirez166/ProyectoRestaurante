6948f2361ce44e1c9d81f1d378056db1b800679e
Elios <eliosjb@outlook.com>
2026-05-27 18:55:34 -0600

    agregando en el ML Restaurante en la rama EliosJan

---
+++ ML/Restaurante.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Restaurante
+    {
+        public int IdRestaurante { get; set; }
+        public string Nombre { get; set; }
+        public byte[] Imagen { get; set; }
+        public DateTime HorarioApertura { get; set; }
+        public DateTime HorarioCierre { get; set; }
+        public string Descripcion { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public bool Estatus { get; set; }
+    }
+}
