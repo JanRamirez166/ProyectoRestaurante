a3e34e07ddf5a226789ffb51ea301f67a498b94e
DayanDSanchez <dayandiegos@gmail.com>
2026-05-28 14:49:11 -0600

    integracion de la merge de De Elios

---
+++ PL/Controllers/RestauranteController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.Controllers
+{
+    public class RestauranteController : Controller
+    {
+        // GET: Restaurante
+        public ActionResult Restaurantes()
+        {
+            return View();
+        }
+
+        public ActionResult Formulario() 
+        {
+            ML.Restaurante restaurante = new ML.Restaurante();
+
+            return View(restaurante);
+        }
+
+        [HttpPost]
+        public ActionResult Formulario(ML.Restaurante restaurante, HttpPostedFileBase Imagen) 
+        {
+
+            return View(restaurante);
+        }
+           
+    }
+}
